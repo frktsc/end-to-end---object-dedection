7923c7375c2f00bef6962843ebc5b84c15cb4baa
frktsc <ftasc40@gmail.com>
2024-05-07 22:00:10 -0400

    files updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'frktsc',
+    author_email= 'ftasc40@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
